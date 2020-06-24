0fa0b10ace4dbe20e2f31786f5fd33f2197a1ff1
William Morrison <williamchrismorrison@gmail.com>
2020-06-24 13:31:05 -0700

    studio complete

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // Get: /user
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get: /user/add
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // Post: /user
+        [HttpPost]
+        [Route("/user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.username = newUser.Username;
+            ViewBag.email = newUser.Email;
+            if (newUser.Password == verify)
+            {
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords do not match";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+
+        public User(string username, string email, string password) : this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
